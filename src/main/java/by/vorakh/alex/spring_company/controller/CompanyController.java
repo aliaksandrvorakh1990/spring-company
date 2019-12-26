a0151ba401953d269339b124044dcfdb9142d518
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2019-12-26 11:46:30 +0300

    edited CompanyController v1.1

--- src/main/java/by/vorakh/alex/spring_company/controller/CompanyController.java
+++ src/main/java/by/vorakh/alex/spring_company/controller/CompanyController.java
@@ -3,41 +3,53 @@ package by.vorakh.alex.spring_company.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import by.vorakh.alex.spring_company.model.entity.Company;
 import by.vorakh.alex.spring_company.repository.service.CompanyService;
 
 
-@RestController 
+@RestController
+@RequestMapping("/project")
 public class CompanyController {
     
     private CompanyService companyService = new CompanyService(); 
     
-    @RequestMapping(value="/companies", method = RequestMethod.GET)
+    @GetMapping("/companies")
     public List<Company> getCompanies() {
 	return companyService.getAll();
     }
     
-    @RequestMapping(value="/company/{id}", method = RequestMethod.GET)
+    @GetMapping(value="/companies/{id}")
     public Company getCompany(@PathVariable("id") Integer id) {
         return companyService.getById(id);
     }
     
-    @RequestMapping(value="/company/new", method = RequestMethod.POST)
-    public void createCompany(Company newCompany) {
+    @PostMapping("/companies")
+    public void createCompany(@Valid @RequestBody Company newCompany) {
 	companyService.create(newCompany);
     }
     
-    @RequestMapping(value="/company/edit/{id}", method = RequestMethod.PUT)
-    public void updateCompany(Company company) {
+    @PutMapping(value="/companies/{id}")
+    public void updateCompany( @PathVariable(value = "id") Integer id, 
+	    @Valid @RequestBody Company editedCompany) {
+	Company company = companyService.getById(id);
+	
+	company.setName(editedCompany.getName());
+	
 	companyService.update(company);
     }
     
-    @RequestMapping(value="/company/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value="/companies/{id}")
     public void updateCompany(@PathVariable("id") Integer id) {
 	companyService.delete(id);
     }
