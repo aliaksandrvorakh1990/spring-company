815bdb27ccc8172c9e1d36d756f9d1b77ae5797b
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-01-07 02:31:35 +0300

    modified spring fox configuration

--- src/main/java/by/vorakh/alex/spring_company/configuration/SpringFoxConfig.java
+++ src/main/java/by/vorakh/alex/spring_company/configuration/SpringFoxConfig.java
@@ -1,12 +1,11 @@
 package by.vorakh.alex.spring_company.configuration;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -23,27 +22,29 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
+          .select()                                 
+          .apis(RequestHandlerSelectors.basePackage("by.vorakh.alex.spring_company"))
+          .paths(PathSelectors.regex("/.*"))                                     
           .build()
-          .apiInfo(apiInfo());                                           
+          .apiInfo(apiInfo())
+          .useDefaultResponseMessages(false)
+          .groupName("Developers");                                           
     }
     
     private ApiInfo apiInfo() {
-	    return new ApiInfo(
-	      "Company REST API", 
-	      "Company is how you store information about human resources. "
-	      + "<br/>Use the Company API calls to manage information about companies "
-	      + "and their employees in your database.", 
-	      "1.0.0", 
-	      "http://localhost:8080/terms_and_conditions", 
-	      new Contact("Alexander Vorakh", 
-		      "https://github.com/aliaksandrvorakh1990/spring-company", 
-		      "aliaksandrvorakh@gmail.com"), 
-	      "The Open Software License 3.0 (OSL-3.0)",
-	      "https://opensource.org/licenses/OSL-3.0",
-	      Collections.emptyList());
+	return new ApiInfoBuilder()
+		.title("Company REST API")
+		.description("Company is how you store information about human resources. " + 
+			"<br/>Use the Company API calls to manage information about companies " + 
+			"and their employees in your database.")
+		.version("1.0.0")
+		.termsOfServiceUrl("http://localhost:8080/terms_and_conditions")
+		.contact(new Contact("Alexander Vorakh", 
+			"https://github.com/aliaksandrvorakh1990/spring-company", 
+			"aliaksandrvorakh@gmail.com"))
+		.license("The Open Software License 3.0 (OSL-3.0)")
+		.licenseUrl("https://opensource.org/licenses/OSL-3.0")
+		.build();
     }
     
 }
