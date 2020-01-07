1187d2578027a62dd202031b8b1ad5d9851233ff
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-01-07 22:39:13 +0300

    company service test getID and create

--- src/test/java/by/vorakh/alex/spring_company/service/CompanyServiceTest.java
+++ src/test/java/by/vorakh/alex/spring_company/service/CompanyServiceTest.java
@@ -1,80 +1,84 @@
 package by.vorakh.alex.spring_company.service;
 
 
+import by.vorakh.alex.spring_company.converter.CompanyToCompanyViewModelConverter;
+import by.vorakh.alex.spring_company.model.payload.CompanyPayload;
+import by.vorakh.alex.spring_company.model.view_model.CompanyViewModel;
+import by.vorakh.alex.spring_company.model.view_model.IdViewModel;
+import by.vorakh.alex.spring_company.repository.CompanyDAO;
+import by.vorakh.alex.spring_company.repository.EmployeeDAO;
+import by.vorakh.alex.spring_company.repository.entity.Company;
+import org.assertj.core.util.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import by.vorakh.alex.spring_company.converter.CompanyToCompanyViewModelConverter;
-import by.vorakh.alex.spring_company.converter.EmployeeToEmployeeViewModelConverter;
-import by.vorakh.alex.spring_company.converter.JobTitleToJobTitleViewModelConverter;
-import by.vorakh.alex.spring_company.converter.PersonalDataToPersonalDataViewModelConverter;
-import by.vorakh.alex.spring_company.converter.SkillToSkillViewModelConverter;
-import by.vorakh.alex.spring_company.model.view_model.CompanyViewModel;
-import by.vorakh.alex.spring_company.repository.CompanyDAO;
-import by.vorakh.alex.spring_company.repository.entity.Company;
-import by.vorakh.alex.spring_company.repository.entity.Employee;
-import by.vorakh.alex.spring_company.repository.entity.JobTitle;
-import by.vorakh.alex.spring_company.repository.entity.PersonalData;
-import by.vorakh.alex.spring_company.repository.entity.Skill;
-
-@RunWith(MockitoJUnitRunner.class)
 public class CompanyServiceTest {
     
-   
+    private static final List<CompanyViewModel> controlList = Lists
+	    .list(new CompanyViewModel(1, "FirstCompany", null));
+    private static final CompanyViewModel controlResult = new CompanyViewModel(2, "TestCompany", null);
+    private static final IdViewModel controlIdViewModel = new IdViewModel(3);
+
     @Mock
-    private SkillToSkillViewModelConverter skillConverter;
-    @Mock
-    private JobTitleToJobTitleViewModelConverter jobTitleConverter;
-    @Mock
-    private PersonalDataToPersonalDataViewModelConverter personalDataConverter; 
-    @Mock
-    private EmployeeToEmployeeViewModelConverter employeeConverter;
-    @Mock
-    private CompanyToCompanyViewModelConverter companyConvertor;
+    private CompanyToCompanyViewModelConverter companyConverter;
+
     @Mock
     private CompanyDAO companyDAO;
+    @Mock
+    private EmployeeDAO employeeDAO;
     @InjectMocks
     private CompanyService service;
-    
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void testGetAll() {
-	PersonalData madMax = new PersonalData(1, "Mad", "Max");
-	JobTitle ceo = new JobTitle(1, "CEO");
+        Company firstCompany = new Company(1, "FirstCompany", null);
+        CompanyViewModel companyViewModel = new CompanyViewModel(1, "FirstCompany", null);
+
+        List<Company> companyList = Lists.list(firstCompany);
+        
+        when(companyConverter.convert(firstCompany)).thenReturn(companyViewModel);
+        when(companyDAO.getAll()).thenReturn(companyList);
+
+        List<CompanyViewModel> resultList = service.getAll();
+       
+        assertEquals(1, resultList.size());
+        assertEquals(controlList, resultList);
+    }
+    
+    @Test
+    public void testGetById() {
+	 Company company = new Company(2, "TestCompany", null);
+	 CompanyViewModel companyViewModel = new CompanyViewModel(2, "TestCompany", null);
+	 
+	 when(companyConverter.convert(company)).thenReturn(companyViewModel);
+	 when(companyDAO.getById(2)).thenReturn(company);
+	 
+	 assertEquals(controlResult,service.getById(2));
+    }
+    
+    @Test
+    public void testCreate() {
+	Company testCompany = new Company(3, "TestCompany", null);
+	CompanyPayload testCompanyPayload = new CompanyPayload(3, "TestCompany", null);
+	when(employeeDAO.getAll(testCompanyPayload.getEmployeeIdList())).thenReturn(null);
+	when(companyDAO.create(testCompany)).thenReturn(3);
 	
-	Skill java = new Skill(1, "Java");
-	
-	List<Skill> ceoSkills = new ArrayList<Skill>();
-	ceoSkills.add(java);
-	
-	Employee madMaxEmp = new Employee(1, madMax, ceo, ceoSkills);
-	List<Employee> employeeListOfFirstCompany = new ArrayList<Employee>();
-	
-	employeeListOfFirstCompany.add(madMaxEmp);
-	
-	Company firstCompany = new Company(1, "FirstCompany", employeeListOfFirstCompany);
-	
-	List<Company> companyList = new ArrayList<Company>();
-	companyList.add(firstCompany);
-	
-	when(jobTitleConverter.convert(ceo)).thenCallRealMethod();
-	when(skillConverter.convert(java)).thenCallRealMethod();
-	when(personalDataConverter.convert(madMax)).thenCallRealMethod();
-	when(employeeConverter.convert(madMaxEmp)).thenCallRealMethod();
-	when(companyConvertor.convert(firstCompany)).thenCallRealMethod();
-	
-	when(companyDAO.getAll()).thenReturn(companyList);
-	
-	List<CompanyViewModel> resultList = service.getAll();
-	assertEquals(1, resultList.size());
+	System.out.println(service.create(testCompanyPayload));
+	assertEquals(controlIdViewModel, service.create(testCompanyPayload));
+    
     }
 
 }
