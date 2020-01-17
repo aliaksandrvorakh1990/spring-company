4aed8a698c20cbf06f06dd34b9b6d92e0e855913
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-01-17 12:58:54 +0300

    allowed CORS for all origins

--- src/main/java/by/vorakh/alex/spring_company/configuration/WebConfig.java
+++ src/main/java/by/vorakh/alex/spring_company/configuration/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+        	.allowedOrigins("*")
         	.allowedMethods("GET", "POST", "PUT", "DELETE")
         	.allowCredentials(true);
     }
