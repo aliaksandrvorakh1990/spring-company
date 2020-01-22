90adfba0711f3c1ce841f8827e25eef954913adb
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-01-22 11:19:57 +0300

    [EDIT-22/01-11:20] updated Skill Service

--- src/main/java/by/vorakh/alex/spring_company/service/SkillService.java
+++ src/main/java/by/vorakh/alex/spring_company/service/SkillService.java
@@ -8,7 +8,9 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import by.vorakh.alex.spring_company.converter.SkillOutsourceToSkillConverter;
 import by.vorakh.alex.spring_company.converter.SkillToSkillViewModelConverter;
+import by.vorakh.alex.spring_company.model.outsource.SkillOutsource;
 import by.vorakh.alex.spring_company.model.payload.SkillPayload;
 import by.vorakh.alex.spring_company.model.view_model.IdViewModel;
 import by.vorakh.alex.spring_company.model.view_model.SkillViewModel;
@@ -25,6 +27,8 @@ public class SkillService implements ServiceInterface<SkillViewModel, SkillPaylo
     private EmployeeDAO employeeDAO;
     @Autowired
     private SkillToSkillViewModelConverter convertor;
+    @Autowired
+    private SkillOutsourceToSkillConverter extermalSourceConvertor;
     
     @Override
     public List<SkillViewModel> getAll() {
@@ -37,22 +41,26 @@ public class SkillService implements ServiceInterface<SkillViewModel, SkillPaylo
 
     @Override
     public SkillViewModel getById(int id) {
+	//TODO 404 if return null
 	return convertor.convert(skillDAO.getById(id));
     }
 
     @Override
     @Transactional
     public IdViewModel create(SkillPayload newPayload) {
-	return new IdViewModel()
-		.setId(skillDAO
-			.create(new Skill(newPayload.getSkillName())));
+	Skill createdSkill = new Skill(newPayload.getSkillName());
+	//TODO 201 if isContained - true
+	int id = skillDAO.create(createdSkill);
+	return new IdViewModel(id);
     }
 
     @Override
     @Transactional
     public void update(SkillPayload editedPayload) {
 	Skill editedSkill = skillDAO.getById(editedPayload.getId());
+	//TODO 404 if return null
 	editedSkill.setSkillName(editedPayload.getSkillName());
+	//TODO 201 if isContained - true (checking if skill exist in DB)
 	skillDAO.update(editedSkill);
     }
 
@@ -60,11 +68,28 @@ public class SkillService implements ServiceInterface<SkillViewModel, SkillPaylo
     @Transactional
     public void delete(int id) {
 	Skill deletedSkill = skillDAO.getById(id);
+	//TODO HOW IMPROVE maybe need to do native SQL query?
 	employeeDAO.getAll(deletedSkill).forEach(emp -> {
 	    emp.getSkillList().remove(deletedSkill);
 	    employeeDAO.update(emp);
 	});
 	skillDAO.delete(deletedSkill);
+    }
+    
+    @Transactional
+    public Skill createAndGet(SkillOutsource extemalSource) {
+	Skill newSkill =extermalSourceConvertor.convert(extemalSource);
+	return createAndGet(newSkill);
+    }
+    
+    @Transactional
+    public Skill createAndGet(Skill newSkill) {
+	//TODO Chech _- IS THERE A GOOD SOLUTION?
+	if (skillDAO.isContained(newSkill)) {
+	    return   skillDAO.findExisted(newSkill);
+	} else {
+	    return skillDAO.createAndGet(newSkill);
+	}
     }
 
 }
