b372c9f0a40f20099187ce544411c73b7917d0d6
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-01-24 11:06:53 +0300

    CRUDEx

---
+++ src/main/java/by/vorakh/alex/spring_company/service/exception/CreateCRUDException.java
@@ -0,0 +1,34 @@
+package by.vorakh.alex.spring_company.service.exception;
+
+public class CreateCRUDException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public CreateCRUDException() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
+
+    public CreateCRUDException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+	super(message, cause, enableSuppression, writableStackTrace);
+	// TODO Auto-generated constructor stub
+    }
+
+    public CreateCRUDException(String message, Throwable cause) {
+	super(message, cause);
+	// TODO Auto-generated constructor stub
+    }
+
+    public CreateCRUDException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+    }
+
+    public CreateCRUDException(Throwable cause) {
+	super(cause);
+	// TODO Auto-generated constructor stub
+    }
+    
+    
+
+}
