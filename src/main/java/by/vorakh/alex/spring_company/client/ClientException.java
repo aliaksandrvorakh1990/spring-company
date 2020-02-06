00f605977bea84f8cef704cdce36ed4ed7a76172
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-02-06 11:51:48 +0300

    [Create-06/02-11-51]

---
+++ src/main/java/by/vorakh/alex/spring_company/client/ClientException.java
@@ -0,0 +1,17 @@
+package by.vorakh.alex.spring_company.client;
+
+public class ClientException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ClientException(String message, Throwable ex) {
+	super(message, ex);
+    }
+
+    public ClientException(String message) {
+	super(message);
+    }
+    
+    
+
+}
